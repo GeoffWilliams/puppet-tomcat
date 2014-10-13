96ac5c8e3bd7faf034bac2da4f93f499b2a25135
Geoff Williams <geoff.williams@puppetlabs.com>
2014-10-13 17:36:54 +1100

    documentation

--- manifests/template_file.pp
+++ manifests/template_file.pp
@@ -1,51 +1,67 @@
 #
 # install a file into a tomcat instance directory based on a template
 #
-#
-# Example
-# tomcat::template_file { "/var/tomcat/instances/myapp/bin/setenv.sh":
-#   templates => $templates,
-#   file_key_map => { "/var/tomcat/instances/myapp/bin/setenv.sh" => 
-#                       "tomcat/bin/setenv.sh.erb" }
-# }
-
-define tomcat::template_file($templates, $file_key_map) {
+define tomcat::template_file($file_params, $file_key_map) {
 
   # Absolute path to the file we want to write
   $filename = $title
 
-  # Corresponding key of fie within the templates map.  This is how
-  # we determine the correct template to use.
+  # Corresponding key of file within the file_params map.  This will be
+  # used to retrieve the parameters and template to use when creating
+  # this file
+  #
   # example: file "/var/lib/tomcat/myapp/bin/setenv.sh" uses the key
-  # "/bin/setenv.sh" in the $templates map.
-  $template_key = $file_key_map[$filename]
+  # "/bin/setenv.sh" in the $file_params map.
+  $key = $file_key_map[$filename]
 
-  # once the key into the $templates map has been obtained, use it
-  # to lookup the correct template file to use
-  #$template_file = $templates[$template_key]
-  $raw_params_hash = $templates[$template_key]
+  # once the key into the $file_params map has been obtained, use it
+  # to lookup the correct (raw) file parameters
+  $raw_params_hash = $file_params[$key]
 
+  # the passed in parameters MUST include a value for template so that
+  # it can be passed to the template function when creating the file
   if (! has_key($raw_params_hash, "template")) {
     fail("Must supply value for template in parameter has for ${template_key} in tomcat::instance")
   }
 
-#  $resource_hash = { "${filename}" => $params_hash }
+  #
+  # build a complete has for ensure_resource()
+  #
+  #
+  # ensure_resource() creates a resource from a hash and is part of stdlib
+  # see reference at:  
+  # https://forge.puppetlabs.com/puppetlabs/stdlib/readme#ensure_resource
+
+  # We need to pass some extra parameters to the hash we pass to 
+  # ensure_resource().
+  #
+  # ensure => file
+  # set here to avoid having to do it for every hash
+  # definition.  It needs to be set to allow the spec tests to pass
+  # 
+  # content => template(...) 
+  # execute the template() function here and store the output of the compiled
+  # .erb file in the hash.  There doesn't seem to be another way to do this
+  # because variables the template relies on will be out of scope if called at
+  # an earlier point such as within params.pp
   $extras_hash = {
     "ensure"  => file,
     "content" => template($raw_params_hash["template"]),
   }
+
+  # We must remove the custom "template" parameter from the hash we send to
+  # ensure_resource() or it will complain.  Delete is part of stdlib, reference:
+  # https://forge.puppetlabs.com/puppetlabs/stdlib/readme#delete
   $params_hash = delete($raw_params_hash, "template")
 
+  # merge our 'fixed' parameter list plus the extra parameters into one big
+  # hash.  Merge is part of stdlib, reference: 
+  # https://forge.puppetlabs.com/puppetlabs/stdlib/readme#merge
   $params =  merge($params_hash, $extras_hash)
 
+  # use ensure_resource to create $filename using $params.  ensure_resource is
+  # part of stdlib, reference: 
+  # https://forge.puppetlabs.com/puppetlabs/stdlib/readme#ensure_resource
   ensure_resource("file", $filename, $params)
   
-#  create_resources("file", merge($resource_hash, $content_hash))
-
-  # Finally, create the file from the template
-  #file { $filename:
-  #  ensure  => file,
-  #  mode    => $params_has
-  #  content => template($params_hash["template"]),
-  #:}
 }
