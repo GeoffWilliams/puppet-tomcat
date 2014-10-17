248211ee8586e3c79c9807b8bc45e1520bc7254d
Geoff Williams <geoff.williams@puppetlabs.com>
2014-10-17 19:07:48 +1100

    test doco

--- spec/defines/library_spec.rb
+++ spec/defines/library_spec.rb
@@ -15,6 +15,7 @@ describe 'tomcat::library', :type => :define do
 
 
   tests = {
+    # install a shared library to default location
     "ensure=>present (shared,default)" => {
       "params" => {
           "ensure"        => "present",
@@ -24,6 +25,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $def_shared_lib_target,
       "trigger" => $def_shared_lib_trigger,
     },
+
+    # install a shared library to custom location
     "ensure=>present (shared,custom)" => {
       "pre_condition" => custom_pre_condition,
       "params" => {
@@ -35,6 +38,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $custom_shared_lib_target,
       "trigger" => $custom_shared_lib_trigger,
     },
+
+    # install an endorsed library to default location
     "ensure=>present (endorsed,default)" => {
       "params" => {
         "ensure"        => "present",
@@ -44,6 +49,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $def_endorsed_lib_target,
       "trigger" => $def_endorsed_lib_trigger,
     },
+
+    # install an endorsed library to custom location
     "ensure=>present (endorsed,custom)" => {
       "pre_condition" => custom_pre_condition,
       "params" => {
@@ -55,6 +62,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $custom_endorsed_lib_target,
       "trigger" => $custom_endorsed_lib_trigger,
     },
+
+    # remove a shared library from default location
     "ensure=>absent (shared,default)" => {
       "params" => {
         "ensure"   => "absent",
@@ -63,6 +72,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $def_shared_lib_target,
       "trigger" => $def_shared_lib_trigger,
     },
+    
+    # remove a shared library from custom location
     "ensure=>absent (shared,custom)" => {
       "pre_condition" => custom_pre_condition,
       "params" => {
@@ -73,6 +84,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $custom_shared_lib_target,
       "trigger" => $custom_shared_lib_trigger,
     },
+
+    # remove an endorsed library from default location
     "ensure=>absent (endorsed,default)" => {
       "params" => {
         "ensure"   => "absent",
@@ -81,6 +94,8 @@ describe 'tomcat::library', :type => :define do
       "target"  => $def_endorsed_lib_target,
       "trigger" => $def_endorsed_lib_trigger,
     },
+
+    # remove an endorsed library from custom location
     "ensure=>absent (endorsed,custom)" => {
       "pre_condition" => custom_pre_condition,
       "params" => {
@@ -95,6 +110,10 @@ describe 'tomcat::library', :type => :define do
 
   tests.each do | test_name, test_data |
     context test_name do
+      # that_comes_before requires any referenced graph nodes already exist
+      # or you get a error:      
+      #   NoMethodError:
+      #     undefined method `[]' for nil:NilClass
       let :pre_condition do
         test_data.has_key?("pre_condition") ? 
           test_data["pre_condition"] : def_pre_condition
@@ -107,14 +126,17 @@ describe 'tomcat::library', :type => :define do
       end
       it {
         if test_data["params"]["ensure"] == "present"
+          # check the nanliu::staging resource to download library was created
           should contain_staging__file($lib_name).with(
             "target" => test_data["target"],
           )
         else
+          # check file scheduled for deltion
           should contain_file(test_data["target"]).with(
             "ensure" => "absent",
           )
         end
+        # check exec created to delete trigger file, thus forcing a restart
         should contain_exec($exec_trigger_title).that_comes_before(
           "File[#{test_data['trigger']}]",
         )
